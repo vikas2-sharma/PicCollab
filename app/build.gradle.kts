f1c394217fc3f0c19a4a99da6dd44225edd2f62c
Vikas Sharma <vikassharma333666@gmail.com>
2025-07-18 21:43:21 +0530

    Add CameraX dependencies to app and core modules
    This commit introduces the necessary dependencies for CameraX in both the `app` and `core` modules.
    
    The following CameraX libraries have been added:
    - `camera-core`
    - `camera-camera2`
    - `camera-lifecycle`
    - `camera-video`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,15 @@ dependencies {
     /*HILT dependencies*/
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
+
+    /*CameraX*/
+
+    // CameraX core library using the camera2 implementation
+    // The following line is optional, as the core library is included indirectly by camera-camera2
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    // If you want to additionally use the CameraX Lifecycle library
+    implementation(libs.androidx.camera.lifecycle)
+    // If you want to additionally use the CameraX VideoCapture library
+    implementation(libs.androidx.camera.video)
 }
