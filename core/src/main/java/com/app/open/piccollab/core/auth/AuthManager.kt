1c52d9688b98db3c31bc09dcc6b8783380c7c187
Vikas Sharma <vikassharma333666@gmail.com>
2025-08-24 00:24:28 +0530

    Refactor Google Sign-In authentication flow
    This commit refactors the Google Sign-In authentication flow in `AuthManager.kt`.
    
    Key changes:
    - Modified `startGoogleAuthentication` to return a `Flow<UserData?>` instead of `Flow<Boolean>`.
    - Introduced `handleCredentialResponse` to process the `GetCredentialResponse`.
    - Utilizes `GoogleIdTokenCredential.createFrom` to extract user data from the `CustomCredential`.
    - Emits a `UserData` object containing user information upon successful authentication, or `null` otherwise.
    - Removed unused commented-out code related to `GetGoogleIdOption`.

--- core/src/main/java/com/app/open/piccollab/core/auth/AuthManager.kt
+++ core/src/main/java/com/app/open/piccollab/core/auth/AuthManager.kt
@@ -3,16 +3,17 @@ package com.app.open.piccollab.core.auth
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
-import androidx.credentials.GetCredentialRequest
 import androidx.credentials.CredentialManager
-import androidx.credentials.GetPasswordOption
-import androidx.credentials.GetPublicKeyCredentialOption
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
 import androidx.credentials.exceptions.NoCredentialException
-import com.app.open.piccollab.core.utils.BundlePrinter
+import com.app.open.piccollab.core.models.user.UserData
 import com.app.open.piccollab.core.utils.WEB_CLIENT_ID
-import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.flow
 import java.security.MessageDigest
 import java.util.UUID
@@ -20,16 +21,10 @@ import java.util.UUID
 private const val TAG = "AuthManager"
 
 object AuthManager {
-    fun startGoogleAuthentication(context: Context): Flow<Boolean> {
+    fun startGoogleAuthentication(context: Context): Flow<UserData?> {
         return flow {
             try {
                 Log.d(TAG, "startGoogleAuthentication() called")
-                /*val signInWithGoogleOption: GetSignInWithGoogleOption = GetSignInWithGoogleOption.Builder(
-                        serverClientId = WEB_CLIENT_ID
-                    ).setNonce(UUID.randomUUID().toString())
-                        .build()
-
-                    Log.d(TAG, "startGoogleAuthentication: ${signInWithGoogleOption.serverClientId}")*/
 
                 val nonce = UUID.randomUUID().toString()
 
@@ -40,13 +35,6 @@ object AuthManager {
                 val hashedNonce = bytesDigest.fold("") { str, it -> str + "%02x".format(it) }
 
                 Log.d(TAG, "startGoogleAuthentication: nonce:  $nonce")
-                /*val googleIdOption: GetGoogleIdOption = GetGoogleIdOption.Builder()
-                    .setFilterByAuthorizedAccounts(false)
-                    .setServerClientId(WEB_CLIENT_ID)
-                    .setAutoSelectEnabled(false)
-                    // nonce string to use when generating a Google ID token
-                    .setNonce(hashedNonce)
-                    .build()*/
 
 
                 val getSignInWithGoogleOption = GetSignInWithGoogleOption.Builder(
@@ -66,22 +54,55 @@ object AuthManager {
                     CredentialManager.create(context = context)
 
                 val response = credentialManager.getCredential(context, getCredentialRequest)
-                Log.d(TAG, "startGoogleAuthentication: res: ${response.credential.data}")
-                emit(!response.credential.data.isEmpty)
 
-
-                BundlePrinter.printBundleContents(response.credential.data)
+                handleCredentialResponse(response)
 
             } catch (e: NoCredentialException) {
                 Toast.makeText(context, "No credential found", Toast.LENGTH_SHORT).show()
                 Log.e(TAG, "startGoogleAuthentication: ", e)
-                emit(false)
+                emit(null)
             } catch (e: Exception) {
                 Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show()
                 Log.e(TAG, "startGoogleAuthentication: ", e)
-                emit(false)
+                emit(null)
             }
 
         }
+    }
+}
+
+private suspend fun FlowCollector<UserData?>.handleCredentialResponse(
+    response: GetCredentialResponse
+) {
+    val credential = response.credential
+    when (credential) {
+        is CustomCredential -> {
+            if (credential.type == GoogleIdTokenCredential.Companion.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL) {
+                val credentialData: GoogleIdTokenCredential =
+                    GoogleIdTokenCredential.createFrom(credential.data)
+                emit(
+                    UserData(
+                        profilePictureUri = credentialData.profilePictureUri,
+                        displayName = credentialData.displayName,
+                        phoneNumber = credentialData.phoneNumber,
+                        id = credentialData.id,
+                        givenName = credentialData.givenName,
+                        idToken = credentialData.idToken,
+                        familyName = credentialData.familyName,
+
+                        )
+                )
+            } else {
+                Log.d(TAG, "startGoogleAuthentication: $credential")
+                emit(null)
+            }
+        }
+
+        else -> {
+            Log.d(TAG, "startGoogleAuthentication: ")
+            emit(null)
+        }
+
+
     }
 }
