8cc0173e021608fb479ed424040c90a0647fe972
Vikas Sharma <vikassharma333666@gmail.com>
2025-08-28 23:01:33 +0530

    feat: Implement basic Scaffold with TopAppBar and BottomNavigation
    This commit introduces a basic Scaffold structure to `MainActivity`.
    
    Key changes:
    - `MainActivity` now uses a `Scaffold` composable.
    - A `TopAppBar` is added, displaying the app title "PicCollab".
    - A placeholder `BottomNavigation` composable is included in the `bottomBar` of the `Scaffold`.
    - The `MainScreen` composable has been simplified by removing the unused `name` parameter.
    - Added a comment with a sample OAuth 2.0 token response structure in the preview function, likely for future reference.

--- app/src/main/java/com/app/open/piccollab/MainActivity.kt
+++ app/src/main/java/com/app/open/piccollab/MainActivity.kt
@@ -7,30 +7,59 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.app.open.piccollab.presentation.common.BottomNavigation
 import com.app.open.piccollab.presentation.ui.login.LoginScreen
 import com.app.open.piccollab.presentation.ui.login.viewmodel.LoginViewmodel
 import com.app.open.piccollab.presentation.ui.profile.ProfileScreen
 import com.app.open.piccollab.ui.theme.PicCollabTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PicCollabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigation()
+                    },
+                    topBar = {
+                        TopAppBar(
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ),
+                            title = {
+                                Text(text = "PicCollab")
+                            }
+                        )
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     val context = LocalContext.current
                     MainScreen(
                         context = context,
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,7 +71,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(
     context: Context,
-    name: String,
     modifier: Modifier = Modifier,
     viewmodel: LoginViewmodel = viewModel<LoginViewmodel>()
 ) {
@@ -57,10 +85,26 @@ fun MainScreen(
     }
 }
 
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PicCollabTheme {
 //        MainScreen("Android")
+
+        /*
+
+         {
+      "access_token": "09eut09ru",
+      "refresh_token_expires_in": 604799,
+      "expires_in": 3599,
+      "token_type": "Bearer",
+      "scope": "https://www.googleapis.com/auth/drive",
+      "refresh_token": "9urt09"
+    }
+
+
+        * */
     }
 }
