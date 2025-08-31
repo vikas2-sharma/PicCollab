97f8d8834ea9ced06c1305ac99ef089cc42e7b4b
Vikas Sharma <vikassharma333666@gmail.com>
2025-08-31 23:10:11 +0530

    Add ButtonWithText composable
    This commit introduces a new reusable composable function `ButtonWithText` in `Components.kt`.
    
    The `ButtonWithText` composable:
    - Takes `text`, `modifier`, and `onClick` as parameters.
    - Displays a `Button` with the provided `text`.
    - Applies padding to the button and the text within it.
    - Sets the text font weight to bold.

--- app/src/main/java/com/app/open/piccollab/presentation/common/Components.kt
+++ app/src/main/java/com/app/open/piccollab/presentation/common/Components.kt
@@ -1,14 +1,20 @@
 package com.app.open.piccollab.presentation.common
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun BottomNavigation() {
@@ -58,5 +64,21 @@ fun BottomNavigation() {
 
             )
 
+    }
+}
+
+@Composable
+fun ButtonWithText(text: String, modifier: Modifier = Modifier, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .padding(20.dp, 5.dp)
+    ) {
+
+        Text(
+            text,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(15.dp, 0.dp)
+        )
     }
 }
