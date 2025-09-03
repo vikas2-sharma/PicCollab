bc8ccba523ad1bf55a4e1da2b6173997ee9cfc98
Vikas Sharma <vikassharma333666@gmail.com>
2025-09-03 23:28:50 +0530

    feat: Introduce RestApiManager for Drive API interaction
    This commit adds the `RestApiManager` class, responsible for handling interactions with the Google Drive API.
    
    Key features:
    - Injects `DriveApiService` for making network requests.
    - Includes a static `accessToken` variable to store the OAuth token.
    - Provides a `getUserDetails` suspend function to fetch user drive details. This function constructs the necessary headers (Authorization and Accept) and delegates the API call to `DriveApiService`.
    - Logs the headers before making the API call for debugging purposes.
    
    The class is annotated with `@Module` and `@InstallIn(SingletonComponent::class)` for Hilt dependency injection.

---
+++ core/src/main/java/com/app/open/piccollab/core/network/module/RestApiManager.kt
@@ -0,0 +1,34 @@
+package com.app.open.piccollab.core.network.module
+
+import android.util.Log
+import com.app.open.piccollab.core.models.user.UserDriveDetail
+import com.app.open.piccollab.core.network.module.apiservices.DriveApiService
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+
+
+private const val TAG = "RestApiManager"
+
+@InstallIn(SingletonComponent::class)
+@Module
+class RestApiManager @Inject constructor(
+    private val driveApiService: DriveApiService
+) {
+
+    companion object {
+        var accessToken: String? = null
+    }
+
+    suspend fun getUserDetails(queryMap: HashMap<String, String>): UserDriveDetail {
+
+        val headerMap = HashMap<String, String>().apply {
+            put("Authorization", "Bearer $accessToken")
+            put("Accept", "application/json")
+        }
+
+        Log.d(TAG, "getUserDetails: $headerMap")
+        return driveApiService.getUserDetails(queryMap, headerMap)
+    }
+}
