4049078ddd520bba1fac69177a972b7cb02e08e0
Vikas Sharma <vikassharma333666@gmail.com>
2025-09-05 23:12:53 +0530

    Introduce Kotlin serialization objects for navigation routes
    This commit adds four new serializable objects to define navigation routes within the application:
    
    - `Login`
    - `Profile`
    - `Search`
    - `Notification`
    
    These objects will be used with a type-safe navigation library for navigating between different screens.

---
+++ app/src/main/java/com/app/open/piccollab/presentation/route/Routes.kt
@@ -0,0 +1,16 @@
+package com.app.open.piccollab.presentation.route
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+object Login
+
+@Serializable
+object Profile
+
+@Serializable
+object Search
+
+@Serializable
+object Notification
