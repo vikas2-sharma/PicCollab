d0d88fa52ae54ec93d8e553858fd0e416a9d006b
Vikas Sharma <vikassharma333666@gmail.com>
2025-09-07 22:56:02 +0530

    feat: Add PrimaryKey to User entity
    This commit adds the `@PrimaryKey` annotation to the `id` field in the `User` entity. This designates the `id` field as the primary key for the `user` table in the Room database.

--- core/src/main/java/com/app/open/piccollab/core/db/entities/User.kt
+++ core/src/main/java/com/app/open/piccollab/core/db/entities/User.kt
@@ -1,6 +1,7 @@
 package com.app.open.piccollab.core.db.entities
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 
 @Entity(tableName = "user")
@@ -8,7 +9,7 @@ data class User(
     val profilePictureUri: String,
     val displayName: String,
     val phoneNumber: String,
-    val id: String,
+    @PrimaryKey val id: String,
     val givenName: String,
     val idToken: String,
     val familyName: String,
