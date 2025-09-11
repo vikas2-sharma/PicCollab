3ace7bef1a389fc8329d729daa91cc29571f1c9d
Vikas Sharma <vikassharma333666@gmail.com>
2025-09-11 23:07:59 +0530

    Log: Add logging to DataStorePref access token methods
    This commit adds `Log.d` statements to the `getAccessToken` and `setAccessToken` methods within the `DataStorePref` class. This will help in debugging and tracing the flow of access token retrieval and storage.

--- core/src/main/java/com/app/open/piccollab/core/db/datastore/DataStorePref.kt
+++ core/src/main/java/com/app/open/piccollab/core/db/datastore/DataStorePref.kt
@@ -1,6 +1,7 @@
 package com.app.open.piccollab.core.db.datastore
 
 import android.content.Context
+import android.util.Log
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
@@ -11,6 +12,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "app_preferences")
+private const val TAG = "DataStorePref"
 
 val ACCESS_TOKEN_KEY = stringPreferencesKey("access_token_key")
 val EXPIRES_IN_KEY = longPreferencesKey("expires_in_key")
@@ -19,12 +21,14 @@ val EXPIRES_IN_KEY = longPreferencesKey("expires_in_key")
 
 class DataStorePref(private val context: Context) {
     fun getAccessToken(): Flow<String> {
+        Log.d(TAG, "getAccessToken() called")
         return context.dataStore.data.map { preferences ->
             preferences[ACCESS_TOKEN_KEY] ?: ""
         }
     }
 
     suspend fun setAccessToken(accessToken: String) {
+        Log.d(TAG, "setAccessToken() called with: accessToken = $accessToken")
         context.dataStore.edit { settings ->
             settings[ACCESS_TOKEN_KEY] = accessToken
         }
