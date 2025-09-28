f9e7ef77f4418ca68624d914fc3281241760498a
Vikas Sharma <vikassharma333666@gmail.com>
2025-09-29 00:11:03 +0530

    feat: Clear event folders from DB on logout
    This commit modifies the `ProfileViewmodel` to ensure that all event folders stored in the local Room database are deleted when a user logs out.
    
    The `logout` function in `ProfileViewmodel` now calls `eventFolderRepository.removeAllFolderFromDB()` to clear the event folder data, in addition to clearing user data and preferences.

--- app/src/main/java/com/app/open/piccollab/presentation/ui/profile/ProfileViewmodel.kt
+++ app/src/main/java/com/app/open/piccollab/presentation/ui/profile/ProfileViewmodel.kt
@@ -1,10 +1,12 @@
 package com.app.open.piccollab.presentation.ui.profile
 
 import android.app.Activity
+import android.util.Printer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.app.open.piccollab.core.auth.AuthManager
 import com.app.open.piccollab.core.db.datastore.DataStorePref
+import com.app.open.piccollab.core.db.room.repositories.EventFolderRepository
 import com.app.open.piccollab.core.db.room.repositories.UserRepository
 import com.app.open.piccollab.core.network.module.drive.DriveManager
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,7 +22,8 @@ class ProfileViewmodel @Inject constructor(
     private val userRepository: UserRepository,
     private val dataStorePref: DataStorePref,
     private val authManager: AuthManager,
-    private val driveManager: DriveManager
+    private val driveManager: DriveManager,
+    private val eventFolderRepository: EventFolderRepository
 ) : ViewModel() {
 
 
@@ -39,6 +42,7 @@ class ProfileViewmodel @Inject constructor(
             dataStorePref.clearAllData()
             userRepository.removeAllUser()
             authManager.logout(activity = activity)
+            eventFolderRepository.removeAllFolderFromDB()
         }
     }
 
