1c797850e851ffe042680d6eb739974f2cb7b599
Vikas Sharma <vikassharma333666@gmail.com>
2025-12-01 23:42:42 +0530

    feat: Add CameraScreen and ViewModel
    This commit introduces the initial structure for the camera feature.
    
    - A new `CameraScreen.kt` composable is created. It uses the `accompanist-permissions` library to check for camera permissions. If permission is granted, it displays a placeholder "Camera" text.
    - A basic `CameraViewModel.kt` file has also been added, currently containing an empty `ViewModel` class.

--- app/src/main/java/com/app/open/piccollab/presentation/ui/camera/CameraScreen.kt
+++ app/src/main/java/com/app/open/piccollab/presentation/ui/camera/CameraScreen.kt
@@ -1,10 +1,38 @@
 package com.app.open.piccollab.presentation.ui.camera
 
-import androidx.camera.core.CameraSelector
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 
+private const val TAG = "CameraScreen"
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun CameraScreen(
+    modifier: Modifier = Modifier
+) {
+    val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
+
+    Log.d(TAG, "CameraScreen: camera permission state: ${cameraPermissionState.status.isGranted}")
+    if (cameraPermissionState.status.isGranted) {
+        ShowCameraPreview(modifier)
+    } else {
+        Log.d(TAG, "CameraScreen: get permission request")
+    }
+}
 
 @Composable
-fun CameraScreen(){
+fun ShowCameraPreview(modifier: Modifier) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Camera")
+    }
 }
---
+++ app/src/main/java/com/app/open/piccollab/presentation/ui/camera/CameraViewmodel.kt
@@ -0,0 +1,8 @@
+package com.app.open.piccollab.presentation.ui.camera
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+/*@HiltViewModel*/
+class CameraViewmodel : ViewModel() {
+}
